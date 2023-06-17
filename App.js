dd85f51fa5c696a606970095bebf435f13e20b59
sim <peachesimran@gmail.com>
2023-06-17 15:01:04 +0800

    edited profile

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Profile from './components/common/profile'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Testing project upload on github</Text>
+      <Profile/>
       <StatusBar style="auto" />
     </View>
   );
